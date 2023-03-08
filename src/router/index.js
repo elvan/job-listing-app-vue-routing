22de6c170d9b4f6489f81c29d65627c87950f783
Elvan Hidayat <elvanhd@gmail.com>
2023-03-08 08:47:53 +0700

    jobs folder structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Jobs from '../views//jobs/Jobs.vue';
 import About from '../views/About.vue';
 import Home from '../views/Home.vue';
 
@@ -11,10 +12,12 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: About,
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs,
   },
 ];
 
