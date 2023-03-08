d3582d68a26046a6744aad9d4b6f9f51c91dd8b4
Elvan Hidayat <elvanhd@gmail.com>
2023-03-08 08:34:00 +0700

    router setup

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <nav>
+  <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  </div>
+  <router-view />
 </template>
 
 <style>
@@ -15,16 +15,16 @@
   color: #2c3e50;
 }
 
-nav {
+#nav {
   padding: 30px;
 }
 
-nav a {
+#nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
-nav a.router-link-exact-active {
+#nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
