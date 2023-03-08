19f69f58fdb8e805e6d4139ee40935ac1eab3716
Elvan Hidayat <elvanhd@gmail.com>
2023-03-08 12:57:26 +0700

    programmatic navigation

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,16 +1,11 @@
 <template>
   <h1>Job Details</h1>
-  <!-- <p>The job id is {{ $route.params.id }}</p> -->
   <p>The job id is {{ id }}</p>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      id: this.$route.params.id,
-    };
-  },
+  props: ['id'],
 };
 </script>
 
